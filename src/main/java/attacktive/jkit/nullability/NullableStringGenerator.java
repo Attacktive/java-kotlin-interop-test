34b42db74d1e4301b810497a99654d64c7632527
Attacktive <welcome.hell.dude@gmail.com>
2022-05-25 16:02:20 +0900

    Remove unnecessary methods
    YAGNI FTW

--- src/main/java/attacktive/jkit/nullability/NullableStringGenerator.java
+++ src/main/java/attacktive/jkit/nullability/NullableStringGenerator.java
@@ -1,55 +1,20 @@
 package attacktive.jkit.nullability;
 
-import java.security.SecureRandom;
-import java.util.Random;
-
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 class NullableStringGenerator {
 	public static final String TEXT = "It's not null.";
-	private static final Random random = new SecureRandom();
 
 	private NullableStringGenerator() { }
-
-	public static String get() {
-		return get(0.5F);
-	}
-
-	public static String get(double probability) {
-		var randomDouble = random.nextDouble();
-		return get(randomDouble <= probability);
-	}
 
 	public static String get(boolean nonNull) {
 		return (nonNull? TEXT: null);
 	}
 
 	@Nullable
-	public static String getNullable() {
-		return getNullable(0.5F);
-	}
-
-	@Nullable
-	public static String getNullable(double probability) {
-		var randomDouble = random.nextDouble();
-		return getNullable(randomDouble <= probability);
-	}
-
-	@Nullable
 	public static String getNullable(boolean nonNull) {
 		return (nonNull? TEXT: null);
-	}
-
-	@NotNull
-	public static String getNotNull() {
-		return getNotNull(0.5F);
-	}
-
-	@NotNull
-	public static String getNotNull(double probability) {
-		var randomDouble = random.nextDouble();
-		return getNotNull(randomDouble <= probability);
 	}
 
 	/**
